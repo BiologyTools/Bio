34e86e288b01653a5abb331954464aa6c38faeea
RepoErik <84872500+BiologyTools@users.noreply.github.com>
2023-01-12 23:54:08 +0200

    Update About.cs

--- Source/About.cs
+++ Source/About.cs
@@ -15,9 +15,6 @@ namespace Bio
         public About()
         {
             InitializeComponent();
-            #if DEBUG
-            MessageBox.Show("Application is running in Debug mode.");
-            #endif
             versionLabel.Text = "Version: " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
         }
 
